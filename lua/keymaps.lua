77d418acbf0f538ba230e249873ab935b553847a
Nikitat0 <nikitatzero@yandex.ru>
2025-02-22 10:53:33 +0700

    Keymap jk

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,3 +4,4 @@ keymap { "<leader>s", "<Cmd>nohlsearch<CR>" }
 keymap { "<Tab>q", "<Cmd>bdelete<CR>" }
 keymap { "<Tab>Q", "<Cmd>bdelete!<CR>" }
 keymap { "<F12>p", "<Cmd>Lazy<CR>" }
+keymap { "jk", "<Esc>", mode = "i" }
